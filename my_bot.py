e65a4db5d2afd961b4b0c3c0ca94026b97b3dc98
Frederik Buur <fbuu@itu.dk>
2025-02-20 08:04:22 +0100

    Revert "buur and  bugge"
    This reverts commit 34b8362824cdbfb59255353c3322b40626179dd0.

--- my_bot.py
+++ my_bot.py
@@ -19,15 +19,6 @@ class Bot:
     trans = {"A":14, "K":13, "Q":12, "J":11, "T":10, "9":9, "8":8, "7":7, "6":6, "5":5, "4":4, "3":3, "2":2}
     return trans.get(char)
 
-  def getCardValueFromHand(self, i):
-    return self.transform(self.obs.my_hand[i][0])
-
-  def getAllCardValues(self, cards):
-    allCards = []
-    for card in cards:
-      allCards.append(self.transform(card[0]))
-    return allCards.sort()
-
   def straight_case(self, cards):
     cards = list(map(lambda el: self.transform(el[0]), cards))
     if 14 in cards:
@@ -35,7 +26,7 @@ class Bot:
     
     count = 0
 
-    for i in range(1, 15):
+    for i in range(1, 15):= 0
       print(f"CARDS: {cards}")
       print("STRAIGHTSHITDAMN")
     return count
@@ -71,31 +62,13 @@ class Bot:
           arr[3] += 1
     return max(arr)
 
-  def getAllCards(self):
-    return self.obs.board_cards + self.obs.my_hand
-
-  def highCardRank(self, card):
-    return self.getAllCardValues(self.getAllCards()).index(card)
-
-  def check_range(self, range):
+  def check_range(self,range):
     return Range(range).is_hand_in_range(self.obs.my_hand)
 
-  def try_raise_pot(self, raise_amount):
+  def try_raise(self, raise_amount):
     if raise_amount*self.obs.get_pot_size() > self.obs.get_my_player_info().stack:
       return self.obs.get_max_raise()
     return raise_amount
-
-  def try_raise_bb(self, raise_amount):
-    if raise_amount*self.obs.big_blind() > self.obs.get_my_player_info().stack:
-      return self.obs.get_max_raise()
-    return raise_amount
-
-  def findPairValue(self):
-    seen = set()
-    for num in self.getAllCardValues(self.getAllCards()):
-        if num in seen:
-            return num
-        seen.add(num)
       
   def pre_flop(self):
     play = 0
@@ -104,161 +77,39 @@ class Bot:
       case 2:
         if self.obs.get_call_size() > 8*self.obs.big_blind:
           if self.check_range("77+, A8s+, KTs+, QJs, AJo+, KQo"):
-            play = self.try_raise_pot(2)
+            play = self.try_raise(2)
         elif self.obs.get_call_size() > 4*self.obs.big_blind:
           if self.check_range("55+, A4s+, K9s+, Q9s+, JTs, A9o+, KTo+, QJo"):
-            play = self.try_raise_pot(2)
+            play = self.try_raise(2)
         elif self.check_range("44+, A2s+, K3s+, Q6s+, J8s+, T8s+, 98s, A4o+, K8o+, Q9o+, JTo"):
-          play = self.try_raise_pot(2)
+          play = self.try_raise(2)
       case 3:
         if self.obs.get_call_size() > 8*self.obs.big_blind:
           if self.check_range("77+, ATs+, KQs, AQo+"):
-            play = self.try_raise_pot(2)
+            play = self.try_raise(2)
         elif self.obs.get_call_size() > 4*self.obs.big_blind:
           if self.check_range("77+, A8s+, KTs+, QJs, AJo+, KQo"):
-            play = self.try_raise_pot(2)
+            play = self.try_raise(2)
         elif self.check_range("55+, A4s+, K9s+, Q9s+, JTs, A9o+, KTo+, QJo"):
-          play = self.try_raise_pot(2)
+          play = self.try_raise(2)
       case _:
         if self.obs.get_call_size() > 4*self.obs.big_blind:
           if self.check_range("77+, ATs+, KQs, AQo+"):
-            play = self.try_raise_pot(2)
+            play = self.try_raise(2)
         elif self.check_range("77+, A8s+, KTs+, QJs, AJo+, KQo"):
-          play = self.try_raise_pot(2)
+          play = self.try_raise(2)
+
     return play # All-in
 
   def post_flop(self):
     play = 0
-
-    boardCards = self.obs.board_cards
-    handHighCard = max(self.getCardValueFromHand(0), self.getCardValueFromHand(1))
-
-    match self.obs.get_player_count():
-      case 2:
-        match self.obs.get_my_hand_type():
-          case 1:
-
-          case 2:
-
-          case 3:
-
-          case 4:
-
-          case 5:
-
-          case 6:
-
-          case 7:
-
-          case 8:
-
-          case 9:
-
-      case _:
-        match self.obs.get_my_hand_type():
-          case 1:
-            if self.flush_cards_count(self.getAllCards()) == 4: #flush
-              if self.straight_case(self.getAllCards()) != 0: #straight
-                if self.obs.get_call_size() > 6*self.obs.big_blind: #calls
-                  return 0
-                else:
-                  return self.try_raise_bb(4)
-              else:
-                if self.highCardRank(handHighCard) == 4:
-                  if self.obs.get_call_size() > 6*self.obs.big_blind: ##calls
-                    return 0
-                  else:
-                    return self.try_raise_bb(4)
-                elif self.highCardRank(handHighCard) == 3:
-                  if self.obs.get_call_size() > 3*self.obs.big_blind: ##calls
-                    return 0
-                  else:
-                    return self.try_raise_bb(4)
-                else:
-                  return 0
-            else:
-              if self.straight_case(self.getAllCards()) != 0: #straight
-                if self.highCardRank(handHighCard) == 4:
-                  if self.obs.get_call_size() > 3 * self.obs.big_blind:  ##calls
-                    return 0
-                  else:
-                    return self.try_raise_bb(4)
-                elif self.highCardRank(handHighCard) == 3:
-                  if self.obs.get_call_size() > 2 * self.obs.big_blind:  ##calls
-                    return 0
-                  else:
-                    return self.try_raise_bb(4)
-                else:
-                  return 0
-              else:
-                if self.highCardRank(handHighCard) == 4:
-                  if self.obs.get_call_size() > 2 * self.obs.big_blind:  ##calls
-                    return 0
-                  else:
-                    return self.try_raise_bb(4)
-          case 2:
-            if self.obs.get_board_hand_type() != 2: #par not on table
-              if self.highCardRank(self.findPairValue()) < 3: #par is highest card
-                if self.flush_cards_count(self.getAllCards()) == 4: #flush
-                  if self.straight_case(self.getAllCards()) != 0: #straight
-                    return self.try_raise_pot(1)
-                  else:
-                    return self.try_raise_pot(0.5)
-                else:
-                  if self.straight_case(self.getAllCards()) != 0: #straight
-                    return self.try_raise_pot(0.5)
-                  else:
-                    if self.obs.get_call_size() > 0.5 * self.obs.get_pot_size():  ##calls
-                      return 0
-                    else:
-                      return self.try_raise_pot(0.5)
-              else:
-                if self.flush_cards_count(self.getAllCards()) == 4: #flush
-                  if self.straight_case(self.getAllCards()) != 0: #straight
-                    if self.obs.get_call_size() > 11 * self.obs.big_blind:  ##calls
-                      return 0
-                    else:
-                      return self.try_raise_pot(0.5)
-                  else:
-                    if self.obs.get_call_size() > 5 * self.obs.big_blind:  ##calls
-                      return 0
-                    else:
-                      return self.try_raise_bb(4)
-                else:
-                  if self.straight_case(self.getAllCards()) != 0: #straight
-                    if self.obs.get_call_size() > 3 * self.obs.big_blind:  ##calls
-                      return 0
-                    else:
-                      return self.try_raise_bb(4)
-                  else:
-                    if self.obs.get_call_size() > 2 * self.obs.big_blind():  ##calls
-                      return 0
-                    else:
-                      return self.try_raise_bb(4)
-            else:
-
-          case 3:
-
-          case 4:
-
-          case 5:
-
-          case 6:
-
-          case 7:
-
-          case 8:
-
-          case 9:
-
-
-
-
     print("WE IN")
-    print(f"FLUSH count board: {self.flush_cards_count(boardCards)}")
-    print(f"FLUSH count full: {self.flush_cards_count(allCards)}")
-    print(self.straight_case(allCards))
+    print(f"FLUSH count board: {self.flush_cards_count(self.obs.board_cards)}")
+    print(f"FLUSH count full: {self.flush_cards_count(self.obs.board_cards + self.obs.my_hand)}")
+    print(self.straight_case(self.obs.board_cards + self.obs.my_hand))
+    return 0
     
+
 
     if self.obs.get_player_count() == 2 or self.obs.get_player_count() == 3:
       x=2
